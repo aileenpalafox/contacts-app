711472619e3dcbdd29f71503384db283d984ae0a
Aileen <aileenpalafox2012@gmail.com>
2021-05-07 11:24:54 -0700

    change in index

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const handler = require("./routes")
 app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
-const uristring = System.getenv("URI")
-//const uristring = "mongodb+srv://admin:admin@cluster0.mywe7.mongodb.net/agenda?retryWrites=true&w=majority";
+
+const uristring = "mongodb+srv://admin:admin@cluster0.mywe7.mongodb.net/agenda?retryWrites=true&w=majority";
 
 mongoose.connect(uristring, {useNewUrlParser: true});
 
