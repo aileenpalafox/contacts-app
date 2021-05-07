427a1d1e74ba65b06a0de7d2ec0a826aa9c03b37
Aileen <aileenpalafox2012@gmail.com>
2021-05-06 17:51:12 -0700

    validations working, filter doesn't differentite lower/upper case

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,13 +1,63 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const validateLetters = function(name) {
+    var re = /^[a-z]+$/i;
+    return re.test(name)
+};
+
+const validateAlphanumeric = function(value){
+    var regex = /^[a-z1-9]+$/i;
+    return  regex.test(value)
+};
+
 const contactsSchema = new Schema({
-    name:  String,
-    lastname: String,
-    company:   String,
-    phone: Number,
-    email: String,
+    name:  {
+        type: String,
+        trim: true,
+        unique: false,
+        required: 'Name is required',
+        validate: [validateLetters, 'Name should contain only letters'],
+        minLength: [2, 'Name should have at least 2 characters'],
+        maxLength: [70, 'Name is too long']
+    },
+    lastname: {
+        type: String,
+        trim: true,
+        required: 'Lastname is required',
+        validate: [validateLetters, 'Lastname should contain only letters'],
+        maxLength: [70, 'Lastname is too long']
+    },
+    company:   {
+        type: String,
+        trim: true,
+        validate: [validateAlphanumeric, 'Company should contain only letters and numbers'],
+        maxLength: [70, 'Lastname is too long']
+    },
+    phone: {
+        type:Number,
+        unique: true,
+        sparse: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : 'Phone should contain only numbers'
+        },
+        maxLength: [15, 'Number is too long']
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        maxLength: [70, 'Lastname is too long'],
+        validate: [validateEmail, 'Email should have the format email@company.some '],
+    }
 });
 
-console.log("ffff")
 module.exports = mongoose.model("Contact",contactsSchema)
